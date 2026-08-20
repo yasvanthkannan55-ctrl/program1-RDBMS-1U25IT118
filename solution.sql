ee9856909d028c87b24b0e081d30137a7198dd4f
YASVANTH KANNAN L <yasvanthkannan55@gmail.com>
2026-08-20 13:50:10 +0530

    program 1

--- solution.sql
+++ solution.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE StudentDB;
-USE StudentDB;
+CREATE DATABASE code1DB;
+USE code1DB;
 CREATE TABLE Department (
       DepartmentID INT(5) PRIMARY KEY,
       DepartmentName VARCHAR(20),
