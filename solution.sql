6a7182b13fe641f30f29da39b5dc4f14a420b27d
YASVANTH KANNAN L <yasvanthkannan55@gmail.com>
2026-08-20 13:36:17 +0530

    program 1 rdbms

--- solution.sql
+++ solution.sql
@@ -1,1 +1,7 @@
-
+CREATE DATABASE CollegeDB;
+USE CollegeDB;
+CREATE TABLE Department (DepartmentID INT(5) PRIMARY KEY,
+                         DepartmentName VARCHAR(20),
+                         HOD VARCHAR(20)
+); 
+DESC Student;
