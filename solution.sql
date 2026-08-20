17ce706641489f9b43a50c20497fdc1e70ad7929
YASVANTH KANNAN L <yasvanthkannan55@gmail.com>
2026-08-20 13:41:18 +0530

    program 1

--- solution.sql
+++ solution.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-CREATE TABLE Department (DepartmentID INT(5) PRIMARY KEY,
-                         DepartmentName VARCHAR(20),
-                         HOD VARCHAR(20)
+CREATE TABLE Department (
+      DepartmentID INT(5) PRIMARY KEY,
+      DepartmentName VARCHAR(20),
+      HOD VARCHAR(20)
 ); 
 DESC Derpartment;
