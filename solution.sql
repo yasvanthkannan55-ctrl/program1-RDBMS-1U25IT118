d23ec43f866ef374ecd27b3fbe803b4092359b14
YASVANTH KANNAN L <yasvanthkannan55@gmail.com>
2026-08-20 13:38:16 +0530

    program 1 rdbms

--- solution.sql
+++ solution.sql
@@ -4,4 +4,4 @@ CREATE TABLE Department (DepartmentID INT(5) PRIMARY KEY,
                          DepartmentName VARCHAR(20),
                          HOD VARCHAR(20)
 ); 
-DESC Student;
+DESC Derpartment;
